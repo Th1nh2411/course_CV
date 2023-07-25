6d007074c9086fa236099cb4b31bcee8088cc0fc
Thinh Nguyen <rinktvn2525@gmail.com>
2023-07-25 23:21:51 +0700

    update carousel

--- src/components/Slide/Slide.js
+++ src/components/Slide/Slide.js
@@ -1,99 +1,43 @@
+import React, { useRef, useState } from 'react';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+import { Pagination, Navigation, Autoplay } from 'swiper/modules';
+
 import styles from './Slide.module.scss';
 import classNames from 'classnames/bind';
-import React, { memo, useEffect, useState } from 'react';
-import images from '../../assets/images';
-import Modal from '../Modal/Modal';
-import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
-import Image from '../Image/Image';
-import { Col, Row } from 'react-bootstrap';
-
 const cx = classNames.bind(styles);
-function splitArrayIntoChunks(arr, chunkSize) {
-    const result = [];
-    let i = 0;
 
-    while (i < arr.length) {
-        const chunk = arr.slice(i, i + chunkSize);
-        result.push(chunk);
-        i += chunkSize;
-    }
-
-    return result;
-}
 function Slide({ children, numItemPerSlide = 1, className, autoPlay = false, numTransitionItem }) {
-    const childrenArray = splitArrayIntoChunks(React.Children.toArray(children), numItemPerSlide);
-    const [sliderCheck, setSliderCheck] = useState(0);
-
-    const handleClickChangeSlide = (value) => {
-        setSliderCheck(value);
-    };
-    useEffect(() => {
-        if (autoPlay) {
-            const slideInterval = setInterval(() => {
-                setSliderCheck(sliderCheck + 1);
-                if (sliderCheck === childrenArray.length - 1) {
-                    setSliderCheck(0);
-                }
-            }, 3000);
-            return () => clearInterval(slideInterval);
-        }
-    }, [sliderCheck]);
-
+    const [thumbsSwiper, setThumbsSwiper] = useState(null);
+    const childrenArray = React.Children.toArray(children);
+    const thumbsPerView = Math.ceil(childrenArray.length / numItemPerSlide);
     return (
-        <div className={cx('slider-wrapper', className)}>
-            <div className={cx('slide-list')}>
-                {childrenArray.map((slide, index) => (
-                    <Row
-                        key={index}
-                        className={cx('slide', 'gx-7')}
-                        src={slide}
-                        style={index === 0 ? { marginLeft: sliderCheck * -100 + '%' } : {}}
-                    >
-                        {slide.map((item, index) => (
-                            <Col key={index} lg={12 / numItemPerSlide}>
-                                <div>{item}</div>
-                            </Col>
-                        ))}
-                    </Row>
-                ))}
-            </div>
-            <div className={cx('slide-dots')}>
-                {childrenArray.map((slider, index) => (
-                    <input
-                        key={index}
-                        onChange={() => handleClickChangeSlide(index)}
-                        value={index}
-                        checked={sliderCheck == index}
-                        type="radio"
-                        name="slide-dot"
-                    />
-                ))}
-            </div>
-            <div
-                onClick={() => {
-                    if (sliderCheck === 0) {
-                        setSliderCheck(0);
-                    } else {
-                        setSliderCheck((prev) => prev - 1);
-                    }
+        <>
+            <Swiper
+                slidesPerView={numItemPerSlide}
+                spaceBetween={50}
+                pagination={{
+                    clickable: true,
                 }}
-                className={cx('left-slide-btn')}
-            >
-                <FiChevronLeft />
-            </div>
-            <div
-                onClick={() => {
-                    if (sliderCheck === childrenArray.length) {
-                        setSliderCheck(0);
-                    } else {
-                        setSliderCheck((prev) => prev + 1);
-                    }
+                autoplay={{
+                    delay: 2500,
+                    disableOnInteraction: false,
                 }}
-                className={cx('right-slide-btn')}
+                navigation={true}
+                modules={[Pagination, Navigation, Autoplay]}
+                speed={1500}
+                className={cx('swiper')}
             >
-                <FiChevronRight />
-            </div>
-        </div>
+                {childrenArray.map((item, index) => (
+                    <SwiperSlide key={index}>{item}</SwiperSlide>
+                ))}
+            </Swiper>
+        </>
     );
 }
 
